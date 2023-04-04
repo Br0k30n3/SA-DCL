d898b91bbd797ac51abe2f87ee1a2cf6f76305c4
Brennan Carroll <brc4rr011@outlook.com>
2023-04-04 12:36:15 -0400

    Add license page

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+
+
+var key = document.getElementById("search__input");
+
+
+key.addEventListener("input", e => {
+  const value = e.target.value;
+  console.log(value)
+})
+$.getJSON("license-db.json", function (data){
+for (let i in data.licenses) {
+  let fName = data.licenses[i].fName;
+  let lName = data.licenses[i].lName;
+  
+}
+
+})
+
+
+
+
+
+
+
+
 function myFunction() {
     var x = document.getElementById("nav-menu");
     if (x.className === "nav-menu") {
@@ -5,4 +30,4 @@ function myFunction() {
     } else {
       x.className = "nav-menu";
     }
-  }
+}
